7f47a579f682832353242bead476a79a13a5db0b
Alexandre Leuck <aleuck2004@gmail.com>
2017-09-27 16:02:31 -0300

    main threads

--- server.cpp
+++ server.cpp
@@ -8,25 +8,39 @@
 #include "serverUI.hpp"
 
 
-void finish(int sig);
+void* server(void* arg);
+void close_server(int sig);
 
 pthread_t thread_ui;
+pthread_t thread_server;
 
 using namespace std;
 
 int main(int argc, char* argv[]) {
   int errCode;
-  (void) signal(SIGINT, finish);
-  cout << "starting interface..." << endl;
+  signal(SIGINT, close_server);
+  cout << "starting service...";
+  errCode = pthread_create(&thread_server, NULL, server, NULL);
+  cout << "starting server interface..." << endl;
   errCode = pthread_create(&thread_ui, NULL, serverUI, NULL);
   if (errCode != 0) {
     cout << "error starting interface" << endl;
     exit(1);
   }
   pthread_join(thread_ui, NULL);
+  pthread_join(thread_server, NULL);
   exit(0);
 }
 
-void finish(int sig) {
-  cout << "server signal " << sig << endl;
+void* server(void* arg) {
+  return NULL;
+}
+
+void close_server(int sig) {
+  kill(thread_ui, sig);
+  pthread_join(thread_ui, NULL);
+  cout << "Stopping service..." << sig << endl;
+  kill(thread_server, sig);
+  pthread_join(thread_server, NULL);
+  exit(0);
 }
